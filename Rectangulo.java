6bb0a971e98a25562eec425bae537ab02ba7a3f1
Jeny Melissa Muñoz Mejia <114088659+jenymunoz@users.noreply.github.com>
2024-12-11 13:24:22 +0100

    corrigiendo errores del método "cambiar datos"

--- Rectangulo.java
+++ Rectangulo.java
@@ -4,47 +4,49 @@ import daw.com.Teclado;
 import daw.com.Pantalla;
 
 public class Rectangulo {
-	//atributos
+
+/*atributos*/
 	private double base;
 	private double altura;
-	private Punto centro;
 	private int color;
+	private Punto centro;
 	
-	//Constructor por defecto
+/*Constructor por defecto*/
 	public Rectangulo() {
-		this.base = 0;
-		this.altura = 0;
+		this.base = 0.0;
+		this.altura = 0.0;
 		this.color = 0;
 		this.centro = new Punto ();
 	}
 	
-	//Constructor con todos los campos. 
-	public Rectangulo(double base, double altura,int color, ) {
+/*Constructor con todos los campos.*/
+	public Rectangulo(double base, double altura,int color, Punto centro) {
 		this.base = base;
 		this.altura = altura;
 		this.color = color;
 		this.centro = centro;
 	}
 	
-	//Constructor de clonado
+/*Constructor de clonado*/
 	public Rectangulo(Rectangulo RectanguloDOS) {
 		this.base = RectanguloDOS.base;
 		this.altura = RectanguloDOS.altura;
-		//this.centro = RectanguloDOS.centro;
 		this.color = RectanguloDOS.color;
+		this.centro = new Punto (RectanguloDOS.centro);
 	}
 	
-	//Metodo mostrar datos
+/*Metodo mostrar datos*/
 	public void mostrarDatos() {
-		//centro.mostrarDatos();
 		Pantalla.escribirString("Base: " + base);
 		Pantalla.escribirSaltoLinea();
 		Pantalla.escribirString("Altura: " + altura);
 		Pantalla.escribirSaltoLinea();
 		Pantalla.escribirInt("Color: " + color, color);
+		Pantalla.escribirSaltoLinea();
+		centro.mostrarDatos();
 	}
 	
-	//get y set
+/*get y set*/
 	public double getBase() {
 		return base;}
 	
@@ -68,15 +70,15 @@ public class Rectangulo {
 		this.color = color;
 	}
 	
-	//public Punto getCentro() {
-		//return centro;
-	//}
+	public Punto getCentro() {
+		return centro;
+	}
 	
-	//public void setCentro(Punto centro) {
-		//this.centro = centro;
-	//}
+	public void setCentro(Punto centro) {
+		this.centro = centro;
+	}
 	
-	//operaciones para el perimetro, area y diagonal
+/*operaciones para el perimetro, area y diagonal*/
 	public double perimetro () {
 		return (double) 2 * (base + altura);
 	}
@@ -90,57 +92,46 @@ public class Rectangulo {
 	}
 	
 	
-	/*Metodo para cambiar datos
-	public void setbase(double nuevaBase) {
-		this.base = nuevaBase;
+/*Metodo para cambiar datos*/
+	public void cambiarDatos() {
+		 do {
+			 Pantalla.escribirString("Indica el nuevo valor de la base:");
+			 String baseNueva = Teclado.leerString();
+			 
+			 if(!baseNueva.isEmpty()) {
+				 try {
+					 this.base=Double.valueOf(baseNueva);
+				 }catch (NumberFormatException e) {
+					 this.base = -1;
+				 }
+			 }
+		 }while (this.altura < 0.0);
+		 
+		 Pantalla.escribirString("indica el valor nuevo de la altura:");
+		 String alturaNueva = Teclado.leerString();
+		 if (!alturaNueva.isEmpty()) {
+			 this.base = Double.valueOf(alturaNueva);
+		 }
+		 
+		 Pantalla.escribirString("Indica el nuevo valor del color: ");
+		 String colorNuevo = Teclado.leerString();
+		 if (!colorNuevo.isEmpty()) {
+			 this.color = Integer.valueOf(colorNuevo);
+		 }
+		 
+		 Pantalla.escribirString("Indica el nuevo valor del punto X: ");
+		 String xNueva = Teclado.leerString();
+		 if (!xNueva.isEmpty())
+			 this.centro.setX(Integer.valueOf(xNueva));
+		 
+		 Pantalla.escribirString("Indica el nuevo valor del punto Y: ");
+		 String yNueva = Teclado.leerString();
+		 if (!yNueva.isEmpty());
+		 	this.centro.setY(Integer.valueOf(yNueva));
+		 	
+		 
 	}
 	
-	public void setaltura(double nuevaAltura) {
-		this.altura = nuevaAltura;
-	}
 	
-	public void setcolor(int nuevoColor) {
-		this.color = nuevoColor;
-	}
-	*/
-	public void cambiarDatos(double nuevaBase,double nuevaAltura, int nuevoColor ) {
-		this.base = nuevaBase;
-		this.altura = nuevaAltura;
-		this.color = nuevoColor; 
-		
-		//centro.leerDatos();
-		setBase (Teclado.leerInt("Base: "));
-		setAltura(Teclado.leerInt("Altura: "));
-		setColor(Teclado.leerInt("Color: "));
-		
-		/*------------------------------------*/
-		
-		//centro.mostrarDatos();
-		Pantalla.escribirString("Base: " + base);
-		Pantalla.escribirSaltoLinea();
-		Pantalla.escribirString("Altura: \n" + altura);
-		Pantalla.escribirSaltoLinea();
-		Pantalla.escribirInt("Color: \n" + color, color);
-
-		/**/
-		/* Estos son apuntes 
-  			
-  		
-    		
-      		*/
-		
-	}
-	
-
-	public static void main(String[] args) {
-		Rectangulo rectanguloEjemplo = new Rectangulo(2.2, 4.4, 255);
-			rectanguloEjemplo.mostrarDatos();
-			
-			Pantalla.escribirSaltoLinea();
-			//modificar los datos
-			rectanguloEjemplo.cambiarDatos(4.4, 5.5, 345);
-			rectanguloEjemplo.mostrarDatos();
-			
-	}
 
 }
